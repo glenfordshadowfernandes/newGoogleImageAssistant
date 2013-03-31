c86b3cdc16ff74d6b5b7077443928866d5470dc8
Glenford <glenfordfernandes@gmail.com>
2013-04-01 00:06:35 +0530

    images are drag able

--- GoogleImageSearch/src/com/example/googleimagesearch/MainActivity.java
+++ GoogleImageSearch/src/com/example/googleimagesearch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.googleimagesearch;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
@@ -12,36 +15,50 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.text.Editable;
+import android.text.method.KeyListener;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.DragShadowBuilder;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ProgressBar;
 
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
 	EditText _search_query_box;
+	AutoCompleteTextView _search_query_auto;
 	String _search_query;
 	String _APIKEY = "AIzaSyBYHDjXAWUoeeIHraRvdenfpUUXQL3c56w";
 	// String _cx = "002151139267678842292:vfsfogsw2da";
@@ -60,10 +77,118 @@ public class MainActivity extends Activity {
 	ArrayList<String> _thumbNails = new ArrayList<String>();
 	static ArrayList<Bitmap> _thumbNailImages = new ArrayList<Bitmap>();
 	static int imgCount = 1;
-	static int progressCount = 95 / _NUM_OF_IMAGES;
+	static int progressCount;
 	GridView imageGrid;
 	ImageAdapter imgAdapter;
+	/*private static final String LOG_TAG = "Autocomplete:"; 
+	private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+	private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+	private static final String OUT_JSON = "/json";
+*/
+	
 
+/*public class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+    private ArrayList<String> resultList;
+   
+    
+    public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+    
+    @Override
+    public int getCount() {
+        return resultList.size();
+    }
+
+    @Override
+    public String getItem(int index) {
+        return resultList.get(index);
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint != null) {
+                    // Retrieve the autocomplete results.
+                    resultList = autocomplete(constraint.toString());
+                    
+                    // Assign the data to the FilterResults
+                    filterResults.values = resultList;
+                    filterResults.count = resultList.size();
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    notifyDataSetChanged();
+                }
+                else {
+                    notifyDataSetInvalidated();
+                }
+            }};
+        return filter;
+    }
+}
+	
+	
+	
+	
+	private ArrayList<String> autocomplete(String input) {
+	    ArrayList<String> resultList = null;
+	    
+	    HttpURLConnection conn = null;
+	    StringBuilder jsonResults = new StringBuilder();
+	    try {
+	        StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+	        sb.append("?sensor=false&key=" + _APIKEY);
+	        sb.append("&components=country:uk");
+	        sb.append("&input=" + URLEncoder.encode(input, "utf8"));
+	        
+	        java.net.URL url = new java.net.URL(sb.toString());
+	        conn = (HttpURLConnection) url.openConnection();
+	        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+	        
+	        // Load the results into a StringBuilder
+	        int read;
+	        char[] buff = new char[1024];
+	        while ((read = in.read(buff)) != -1) {
+	            jsonResults.append(buff, 0, read);
+	        }
+	    } catch (MalformedURLException e) {
+	        Log.e(LOG_TAG, "Error processing Places API URL", e);
+	        return resultList;
+	    } catch (IOException e) {
+	        Log.e(LOG_TAG, "Error connecting to Places API", e);
+	        return resultList;
+	    } finally {
+	        if (conn != null) {
+	            conn.disconnect();
+	        }
+	    }
+
+	    try {
+	        // Create a JSON object hierarchy from the results
+	        JSONObject jsonObj = new JSONObject(jsonResults.toString());
+	        JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+	        
+	        // Extract the Place descriptions from the results
+	        resultList = new ArrayList<String>(predsJsonArray.length());
+	        for (int i = 0; i < predsJsonArray.length(); i++) {
+	            resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+	        }
+	    } catch (JSONException e) {
+	        Log.e(LOG_TAG, "Cannot process JSON results", e);
+	    }
+	    
+	    return resultList;
+	}
+*/	
+	
 	
 	//IMAGE ADAPTER CLASS
 	public class ImageAdapter extends BaseAdapter {
@@ -111,6 +236,7 @@ public class MainActivity extends Activity {
 			imageView.setImageBitmap(_thumbNailImages_TEMP.get(position));
 			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			imageView.setPadding(5, 5, 5, 5);
+			imageView.setOnTouchListener(new MyTouchListener());
 			Log.i("Google Image Search",
 					"Image View Creation = " + imageView.getHeight());
 			return arg1;
@@ -124,15 +250,16 @@ public class MainActivity extends Activity {
 
 		protected void onPostExecute(Bitmap imagePic) {
 
-			// Log.i("Google Image Search"
-			// ,"Thumbnail onPostExecute Link = "+ThumbnailLink);
-
 			_thumbNailImages.add(imagePic);
 			Log.i("Google Image Search", "ThumbnailImagesArraySize = "
 					+ _thumbNailImages.size());
 
 			publishProgress(progressCount);
-			progressCount = progressCount + progressCount;
+			
+			Log.i("Google Image Search", "Progress count = "
+					+ progressCount);
+			
+			progressCount = progressCount + 10;
 
 			if (_thumbNailImages.size() == _NUM_OF_IMAGES) {
 				imgAdapter = new ImageAdapter(MainActivity.this,
@@ -279,7 +406,7 @@ public class MainActivity extends Activity {
 				Log.i("Google Image Search Error", e.toString());
 
 			}
-			publishProgress(5);
+			//publishProgress(10);
 			return _thumbNails;
 		}
 
@@ -294,9 +421,51 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		_search_query_box = (EditText) findViewById(R.id._SEARCH_QUERY);
+		//_search_query_auto = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);		
 		_search_button = (Button) findViewById(R.id.SearchButton);
 		_search_progress_bar = (ProgressBar) findViewById(R.id._SEARCH_PROGRESS_BAR);
-
+		
+		/*_search_query_auto.setKeyListener(new KeyListener() {
+			
+			@Override
+			public boolean onKeyUp(View arg0, Editable arg1, int arg2, KeyEvent arg3) {
+				// TODO Auto-generated method stub
+				
+				Log.i(LOG_TAG, "Entered on key up function");
+				Log.i(LOG_TAG, "Entered on key ="+_search_query_auto.getText().toString());
+				autocomplete(_search_query_auto.getText().toString());
+				return true;
+			}
+			
+			@Override
+			public boolean onKeyOther(View arg0, Editable arg1, KeyEvent arg2) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean onKeyDown(View arg0, Editable arg1, int arg2, KeyEvent arg3) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public int getInputType() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public void clearMetaKeyState(View arg0, Editable arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		_search_query_auto.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autolist));
+*/		
+		
+		
 		_search_button.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -308,8 +477,11 @@ public class MainActivity extends Activity {
 						+ _search_query + "&num=" + _NUM_OF_IMAGES + "&cx="
 						+ _cx + "&fileType=png&searchType=image&imgSize="
 						+ _IMAGE_SIZE + "&key=" + _APIKEY + "&alt=json";
-
+				
+				progressCount = 100 / _NUM_OF_IMAGES;
 				_thumbNailImages.removeAll(_thumbNailImages);
+				Log.i("Google Image Search", "ThumbnailImagesArraySizeAfterClick = "
+						+ _thumbNailImages.size());
 				new httpRequest().execute(URL);
 
 			}
@@ -323,5 +495,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
+	public final class MyTouchListener implements OnTouchListener {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+	      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+	        ClipData data = ClipData.newPlainText("", "");
+	        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+	        view.startDrag(data, shadowBuilder, view, 0);
+	        view.setVisibility(View.INVISIBLE);
+	        return true;
+	      } else {
+	        return false;
+	      }
+	    }
+
+	  }
+
 
 }
+
