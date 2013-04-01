87bdb273aecc739044cbd54d3b55a522c9ad4c5f
Glenford <glenfordfernandes@gmail.com>
2013-04-01 15:36:22 +0530

    Share using available media started

--- GoogleImageSearch/src/com/example/googleimagesearch/MainActivity.java
+++ GoogleImageSearch/src/com/example/googleimagesearch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.googleimagesearch;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -25,6 +26,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.Editable;
@@ -35,6 +37,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnClickListener;
@@ -80,118 +83,85 @@ public class MainActivity extends Activity {
 	static int progressCount;
 	GridView imageGrid;
 	ImageAdapter imgAdapter;
-	/*private static final String LOG_TAG = "Autocomplete:"; 
-	private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
-	private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
-	private static final String OUT_JSON = "/json";
-*/
-	
 
-/*public class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
-    private ArrayList<String> resultList;
-   
-    
-    public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
-        super(context, textViewResourceId);
-    }
-    
-    @Override
-    public int getCount() {
-        return resultList.size();
-    }
+	/*
+	 * private static final String LOG_TAG = "Autocomplete:"; private static
+	 * final String PLACES_API_BASE =
+	 * "https://maps.googleapis.com/maps/api/place"; private static final String
+	 * TYPE_AUTOCOMPLETE = "/autocomplete"; private static final String OUT_JSON
+	 * = "/json";
+	 */
 
-    @Override
-    public String getItem(int index) {
-        return resultList.get(index);
-    }
+	/*
+	 * public class PlacesAutoCompleteAdapter extends ArrayAdapter<String>
+	 * implements Filterable { private ArrayList<String> resultList;
+	 * 
+	 * 
+	 * public PlacesAutoCompleteAdapter(Context context, int textViewResourceId)
+	 * { super(context, textViewResourceId); }
+	 * 
+	 * @Override public int getCount() { return resultList.size(); }
+	 * 
+	 * @Override public String getItem(int index) { return
+	 * resultList.get(index); }
+	 * 
+	 * @Override public Filter getFilter() { Filter filter = new Filter() {
+	 * 
+	 * @Override protected FilterResults performFiltering(CharSequence
+	 * constraint) { FilterResults filterResults = new FilterResults(); if
+	 * (constraint != null) { // Retrieve the autocomplete results. resultList =
+	 * autocomplete(constraint.toString());
+	 * 
+	 * // Assign the data to the FilterResults filterResults.values =
+	 * resultList; filterResults.count = resultList.size(); } return
+	 * filterResults; }
+	 * 
+	 * @Override protected void publishResults(CharSequence constraint,
+	 * FilterResults results) { if (results != null && results.count > 0) {
+	 * notifyDataSetChanged(); } else { notifyDataSetInvalidated(); } }}; return
+	 * filter; } }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * private ArrayList<String> autocomplete(String input) { ArrayList<String>
+	 * resultList = null;
+	 * 
+	 * HttpURLConnection conn = null; StringBuilder jsonResults = new
+	 * StringBuilder(); try { StringBuilder sb = new
+	 * StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+	 * sb.append("?sensor=false&key=" + _APIKEY);
+	 * sb.append("&components=country:uk"); sb.append("&input=" +
+	 * URLEncoder.encode(input, "utf8"));
+	 * 
+	 * java.net.URL url = new java.net.URL(sb.toString()); conn =
+	 * (HttpURLConnection) url.openConnection(); InputStreamReader in = new
+	 * InputStreamReader(conn.getInputStream());
+	 * 
+	 * // Load the results into a StringBuilder int read; char[] buff = new
+	 * char[1024]; while ((read = in.read(buff)) != -1) {
+	 * jsonResults.append(buff, 0, read); } } catch (MalformedURLException e) {
+	 * Log.e(LOG_TAG, "Error processing Places API URL", e); return resultList;
+	 * } catch (IOException e) { Log.e(LOG_TAG,
+	 * "Error connecting to Places API", e); return resultList; } finally { if
+	 * (conn != null) { conn.disconnect(); } }
+	 * 
+	 * try { // Create a JSON object hierarchy from the results JSONObject
+	 * jsonObj = new JSONObject(jsonResults.toString()); JSONArray
+	 * predsJsonArray = jsonObj.getJSONArray("predictions");
+	 * 
+	 * // Extract the Place descriptions from the results resultList = new
+	 * ArrayList<String>(predsJsonArray.length()); for (int i = 0; i <
+	 * predsJsonArray.length(); i++) {
+	 * resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+	 * } } catch (JSONException e) { Log.e(LOG_TAG,
+	 * "Cannot process JSON results", e); }
+	 * 
+	 * return resultList; }
+	 */
 
-    @Override
-    public Filter getFilter() {
-        Filter filter = new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults filterResults = new FilterResults();
-                if (constraint != null) {
-                    // Retrieve the autocomplete results.
-                    resultList = autocomplete(constraint.toString());
-                    
-                    // Assign the data to the FilterResults
-                    filterResults.values = resultList;
-                    filterResults.count = resultList.size();
-                }
-                return filterResults;
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                if (results != null && results.count > 0) {
-                    notifyDataSetChanged();
-                }
-                else {
-                    notifyDataSetInvalidated();
-                }
-            }};
-        return filter;
-    }
-}
-	
-	
-	
-	
-	private ArrayList<String> autocomplete(String input) {
-	    ArrayList<String> resultList = null;
-	    
-	    HttpURLConnection conn = null;
-	    StringBuilder jsonResults = new StringBuilder();
-	    try {
-	        StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
-	        sb.append("?sensor=false&key=" + _APIKEY);
-	        sb.append("&components=country:uk");
-	        sb.append("&input=" + URLEncoder.encode(input, "utf8"));
-	        
-	        java.net.URL url = new java.net.URL(sb.toString());
-	        conn = (HttpURLConnection) url.openConnection();
-	        InputStreamReader in = new InputStreamReader(conn.getInputStream());
-	        
-	        // Load the results into a StringBuilder
-	        int read;
-	        char[] buff = new char[1024];
-	        while ((read = in.read(buff)) != -1) {
-	            jsonResults.append(buff, 0, read);
-	        }
-	    } catch (MalformedURLException e) {
-	        Log.e(LOG_TAG, "Error processing Places API URL", e);
-	        return resultList;
-	    } catch (IOException e) {
-	        Log.e(LOG_TAG, "Error connecting to Places API", e);
-	        return resultList;
-	    } finally {
-	        if (conn != null) {
-	            conn.disconnect();
-	        }
-	    }
-
-	    try {
-	        // Create a JSON object hierarchy from the results
-	        JSONObject jsonObj = new JSONObject(jsonResults.toString());
-	        JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
-	        
-	        // Extract the Place descriptions from the results
-	        resultList = new ArrayList<String>(predsJsonArray.length());
-	        for (int i = 0; i < predsJsonArray.length(); i++) {
-	            resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
-	        }
-	    } catch (JSONException e) {
-	        Log.e(LOG_TAG, "Cannot process JSON results", e);
-	    }
-	    
-	    return resultList;
-	}
-*/	
-	
-	
-	//IMAGE ADAPTER CLASS
-	public class ImageAdapter extends BaseAdapter {
+	// IMAGE ADAPTER CLASS
+	public class ImageAdapter extends BaseAdapter implements OnLongClickListener {
 
 		private Context mContext;
 		ArrayList<Bitmap> _thumbNailImages_TEMP;
@@ -236,10 +206,27 @@ public class MainActivity extends Activity {
 			imageView.setImageBitmap(_thumbNailImages_TEMP.get(position));
 			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			imageView.setPadding(5, 5, 5, 5);
-			imageView.setOnTouchListener(new MyTouchListener());
+			//imageView.setOnTouchListener(new MyTouchListener());
+			imageView.setOnLongClickListener(this);
 			Log.i("Google Image Search",
 					"Image View Creation = " + imageView.getHeight());
 			return arg1;
+		}
+
+		@Override
+		public boolean onLongClick(View v) {
+			// TODO Auto-generated method stub
+			
+			Bitmap shareImage = v.getDrawingCache();
+			/*ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			shareImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+			byte[] byteArray = stream.toByteArray();*/
+			Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+			sharingIntent.setType("image/png");
+			sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,shareImage);
+			startActivity(Intent.createChooser(sharingIntent,"Share Image using"));
+			
+			return false;
 		}
 
 	}
@@ -255,10 +242,9 @@ public class MainActivity extends Activity {
 					+ _thumbNailImages.size());
 
 			publishProgress(progressCount);
-			
-			Log.i("Google Image Search", "Progress count = "
-					+ progressCount);
-			
+
+			Log.i("Google Image Search", "Progress count = " + progressCount);
+
 			progressCount = progressCount + 10;
 
 			if (_thumbNailImages.size() == _NUM_OF_IMAGES) {
@@ -379,15 +365,17 @@ public class MainActivity extends Activity {
 
 						JSONObject _IMAGE_NAME_OBJ = _IMAGE_RESULTS
 								.getJSONObject(i);
-						
-						//TO GET PROPER IMAGES
-						imageLink = _IMAGE_NAME_OBJ.getString("link");
-						
-						//TO GET THUMBNAILS OF IMAGES
-						/*JSONObject tempImageObj = _IMAGE_NAME_OBJ
-								.getJSONObject("image");
 
-						imageLink = tempImageObj.getString("thumbnailLink");*/
+						// TO GET PROPER IMAGES
+						imageLink = _IMAGE_NAME_OBJ.getString("link");
+
+						// TO GET THUMBNAILS OF IMAGES
+						/*
+						 * JSONObject tempImageObj = _IMAGE_NAME_OBJ
+						 * .getJSONObject("image");
+						 * 
+						 * imageLink = tempImageObj.getString("thumbnailLink");
+						 */
 
 						_thumbNails.add(imageLink);
 
@@ -406,7 +394,7 @@ public class MainActivity extends Activity {
 				Log.i("Google Image Search Error", e.toString());
 
 			}
-			//publishProgress(10);
+			// publishProgress(10);
 			return _thumbNails;
 		}
 
@@ -419,53 +407,42 @@ public class MainActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
-		
+
 		_search_query_box = (EditText) findViewById(R.id._SEARCH_QUERY);
-		//_search_query_auto = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);		
+		// _search_query_auto =
+		// (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
 		_search_button = (Button) findViewById(R.id.SearchButton);
 		_search_progress_bar = (ProgressBar) findViewById(R.id._SEARCH_PROGRESS_BAR);
-		
-		/*_search_query_auto.setKeyListener(new KeyListener() {
-			
-			@Override
-			public boolean onKeyUp(View arg0, Editable arg1, int arg2, KeyEvent arg3) {
-				// TODO Auto-generated method stub
-				
-				Log.i(LOG_TAG, "Entered on key up function");
-				Log.i(LOG_TAG, "Entered on key ="+_search_query_auto.getText().toString());
-				autocomplete(_search_query_auto.getText().toString());
-				return true;
-			}
-			
-			@Override
-			public boolean onKeyOther(View arg0, Editable arg1, KeyEvent arg2) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean onKeyDown(View arg0, Editable arg1, int arg2, KeyEvent arg3) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public int getInputType() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public void clearMetaKeyState(View arg0, Editable arg1, int arg2) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		_search_query_auto.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autolist));
-*/		
-		
-		
+
+		/*
+		 * _search_query_auto.setKeyListener(new KeyListener() {
+		 * 
+		 * @Override public boolean onKeyUp(View arg0, Editable arg1, int arg2,
+		 * KeyEvent arg3) { // TODO Auto-generated method stub
+		 * 
+		 * Log.i(LOG_TAG, "Entered on key up function"); Log.i(LOG_TAG,
+		 * "Entered on key ="+_search_query_auto.getText().toString());
+		 * autocomplete(_search_query_auto.getText().toString()); return true; }
+		 * 
+		 * @Override public boolean onKeyOther(View arg0, Editable arg1,
+		 * KeyEvent arg2) { // TODO Auto-generated method stub return false; }
+		 * 
+		 * @Override public boolean onKeyDown(View arg0, Editable arg1, int
+		 * arg2, KeyEvent arg3) { // TODO Auto-generated method stub return
+		 * false; }
+		 * 
+		 * @Override public int getInputType() { // TODO Auto-generated method
+		 * stub return 0; }
+		 * 
+		 * @Override public void clearMetaKeyState(View arg0, Editable arg1, int
+		 * arg2) { // TODO Auto-generated method stub
+		 * 
+		 * } });
+		 * 
+		 * _search_query_auto.setAdapter(new PlacesAutoCompleteAdapter(this,
+		 * R.layout.autolist));
+		 */
+
 		_search_button.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -473,15 +450,18 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated method stub
 
 				_search_query = _search_query_box.getText().toString();
+
 				URL = "https://www.googleapis.com/customsearch/v1?q="
 						+ _search_query + "&num=" + _NUM_OF_IMAGES + "&cx="
 						+ _cx + "&fileType=png&searchType=image&imgSize="
 						+ _IMAGE_SIZE + "&key=" + _APIKEY + "&alt=json";
-				
+
 				progressCount = 100 / _NUM_OF_IMAGES;
 				_thumbNailImages.removeAll(_thumbNailImages);
-				Log.i("Google Image Search", "ThumbnailImagesArraySizeAfterClick = "
-						+ _thumbNailImages.size());
+				Log.i("Google Image Search",
+						"ThumbnailImagesArraySizeAfterClick = "
+								+ _thumbNailImages.size());
+
 				new httpRequest().execute(URL);
 
 			}
@@ -495,24 +475,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
-	public final class MyTouchListener implements OnTouchListener {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-	      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-	        ClipData data = ClipData.newPlainText("", "");
-	        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-	        view.startDrag(data, shadowBuilder, view, 0);
-	        view.setVisibility(View.INVISIBLE);
-	        return true;
-	      } else {
-	        return false;
-	      }
-	    }
 
-	  }
-
+	
 
 }
-
